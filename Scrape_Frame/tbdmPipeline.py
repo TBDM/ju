14b10d80704f26d7b275c54025eae0f60e5a7c92
Otomadmt <otomadmt@icloud.com>
2017-04-08 20:57:10 +0800

    * Fix indent in tbdmPipeline

--- Scrape_Frame/tbdmPipeline.py
+++ Scrape_Frame/tbdmPipeline.py
@@ -23,12 +23,12 @@ from tbdmSlack import tbdmSlack
 
 """
 @param:
-	tbdmDb 		tbdmDatabase instance.
-	logger		tbdmLogger instance.
-	mongoCli	MongoDB connections. This can be maintained by the manager in future, 
-						'cause pymongo(3.3.0) instances are thread-safe (but not fork-safe). 
-	redis0Cli	Redis connections to db0. This can be maintained by the manager in future, 
-						'cause py-redis(2.10.5) instances are thread-safe. 
+    tbdmDb         tbdmDatabase instance.
+    logger        tbdmLogger instance.
+    mongoCli    MongoDB connections. This can be maintained by the manager in future, 
+                        'cause pymongo(3.3.0) instances are thread-safe (but not fork-safe). 
+    redis0Cli    Redis connections to db0. This can be maintained by the manager in future, 
+                        'cause py-redis(2.10.5) instances are thread-safe. 
 """
 #Workers maintain their own connections
 tbdmDb = tbdmDatabase()
@@ -44,81 +44,81 @@ NO_TASK_HALT = 300
 
 #----------class definition----------
 class workPipeline():
-	"""
-	Scrape pipeline for TBDM Project.
-	!! This class must be instantiated before use.
-	@pythonVersion: 3.5.2
-	@methods:
-	tasker 		Acquires tasks for this worker and sets up a 
-				file lock for recovering.
-	postman		Checks for last fail records and sends them back 
-				to redis after task acquiring.
-	worker 		Does the work and record what have been done. 
-	@author: X.Huang, P.Liu
-	@maintenance: X.Huang, P.Liu
-	@creation: 2016-11-8
-	@modified: 2016-11-8
-	@version: 0.01-alpha
-	"""
-	tasknum = tbdmConfig.TASK_NUM;
-	time_wait_flag = True
-	worker = tbdmscraper.Worker()
+    """
+    Scrape pipeline for TBDM Project.
+    !! This class must be instantiated before use.
+    @pythonVersion: 3.5.2
+    @methods:
+    tasker         Acquires tasks for this worker and sets up a 
+                file lock for recovering.
+    postman        Checks for last fail records and sends them back 
+                to redis after task acquiring.
+    worker         Does the work and record what have been done. 
+    @author: X.Huang, P.Liu
+    @maintenance: X.Huang, P.Liu
+    @creation: 2016-11-8
+    @modified: 2016-11-8
+    @version: 0.01-alpha
+    """
+    tasknum = tbdmConfig.TASK_NUM;
+    time_wait_flag = True
+    worker = tbdmscraper.Worker()
 
-	def tasker(self):
-		if os.path.exists("tbdmPipelock.lock"):
-			logger.warning("Lock file detected, recovering from lock.")
-			with open("tbdmPipelock.lock", "rb") as f:
-				tasklist = pickle.load(f)
-		else:
-			with redisCli.pipeline() as poppipe:
-				while True:
-					try:
-						#Starting transaction
-						poppipe.watch('juList')
-						tasklist = poppipe.zrange('juList', 0, self.tasknum - 1)
-						if tasklist:
-							poppipe.multi()
-							for item in tasklist:
-								poppipe.zrem('juList', item)
-						else:
-							logger.warning("Redis returned empty list, halting worker.")
-							slacker.post_message("Redis returned empty list, halting worker for " + str(NO_TASK_HALT)
-												 + " seconds.")
-							time.sleep(NO_TASK_HALT)
-							continue
-							#Redis returned no task
-						poppipe.execute()
-						self.worker.task_locker(tasklist)
-						break
-					except redis.exceptions.WatchError as _Ewatch:
-						logger.warning("Redis watch error, retry fetching.")
-						time.sleep(random.uniform(3, 5))
-						continue
-					except Exception as _Eall:
-						logger.error(_Eall)
-		return tasklist
+    def tasker(self):
+        if os.path.exists("tbdmPipelock.lock"):
+            logger.warning("Lock file detected, recovering from lock.")
+            with open("tbdmPipelock.lock", "rb") as f:
+                tasklist = pickle.load(f)
+        else:
+            with redisCli.pipeline() as poppipe:
+                while True:
+                    try:
+                        #Starting transaction
+                        poppipe.watch('juList')
+                        tasklist = poppipe.zrange('juList', 0, self.tasknum - 1)
+                        if tasklist:
+                            poppipe.multi()
+                            for item in tasklist:
+                                poppipe.zrem('juList', item)
+                        else:
+                            logger.warning("Redis returned empty list, halting worker.")
+                            slacker.post_message("Redis returned empty list, halting worker for " + str(NO_TASK_HALT)
+                                                 + " seconds.")
+                            time.sleep(NO_TASK_HALT)
+                            continue
+                            #Redis returned no task
+                        poppipe.execute()
+                        self.worker.task_locker(tasklist)
+                        break
+                    except redis.exceptions.WatchError as _Ewatch:
+                        logger.warning("Redis watch error, retry fetching.")
+                        time.sleep(random.uniform(3, 5))
+                        continue
+                    except Exception as _Eall:
+                        logger.error(_Eall)
+        return tasklist
 
-	def manager(self):
-		"""
-		Tasker acquires tasks for this worker and sets up a file lock for recovering.
-		@param:
-				tasknum		Tasks to be executed at one run, no more than 100.
-		"""
-		taskstrs = self.tasker()
-		taskdicts = self.worker.task_strs2dicts(taskstrs)
-		if(self.worker.task_min_score(taskdicts) > time.time()):
-			sleeptime = int((self.worker.task_min_score(taskdicts) - time.time()) % 420) + 30
-			if(self.time_wait_flag):
-				logger.warning("Worker halts waiting for latest tasktime for " + str(sleeptime) + " seconds.")
-				self.time_wait_flag = False
-			self.worker.task_back2redis(taskdicts)
-			time.sleep(sleeptime)
-		else:
-			if(not self.time_wait_flag):
-				logger.warning("Worker continued from task time waiting.")
-				self.time_wait_flag = True
-			(success_cnt, total_cnt) = self.worker.request_page(taskdicts)
-			logger.warning("Worker finished " + str(success_cnt) + " out of " + str(total_cnt))
+    def manager(self):
+        """
+        Tasker acquires tasks for this worker and sets up a file lock for recovering.
+        @param:
+                tasknum        Tasks to be executed at one run, no more than 100.
+        """
+        taskstrs = self.tasker()
+        taskdicts = self.worker.task_strs2dicts(taskstrs)
+        if(self.worker.task_min_score(taskdicts) > time.time()):
+            sleeptime = int((self.worker.task_min_score(taskdicts) - time.time()) % 420) + 30
+            if(self.time_wait_flag):
+                logger.warning("Worker halts waiting for latest tasktime for " + str(sleeptime) + " seconds.")
+                self.time_wait_flag = False
+            self.worker.task_back2redis(taskdicts)
+            time.sleep(sleeptime)
+        else:
+            if(not self.time_wait_flag):
+                logger.warning("Worker continued from task time waiting.")
+                self.time_wait_flag = True
+            (success_cnt, total_cnt) = self.worker.request_page(taskdicts)
+            logger.warning("Worker finished " + str(success_cnt) + " out of " + str(total_cnt))
 
 #----------class definition----------
 
@@ -135,17 +135,17 @@ if __name__ == "__main__":
         # Check for Config
         print("Tell me who you are in tbdmConfig before starting pipeline.")
     else:
-		workpipe = workPipeline()
-		while (not os.path.exists('stopPipe')):
-			try:
-				workpipe.manager()
-			except Exception as _Eall:
-				logger.critical("Manager reported an error " + str(_Eall))
-				time.sleep(3)
-			else:
-				logger.info("Manager finished one round.")
-		tbdmscraper.display.close()
-		logger.warning("Manager stopped on detecting flag.")
-		slacker.post_message("Manager stopped on detecting flag.")
+        workpipe = workPipeline()
+        while (not os.path.exists('stopPipe')):
+            try:
+                workpipe.manager()
+            except Exception as _Eall:
+                logger.critical("Manager reported an error " + str(_Eall))
+                time.sleep(3)
+            else:
+                logger.info("Manager finished one round.")
+        tbdmscraper.display.close()
+        logger.warning("Manager stopped on detecting flag.")
+        slacker.post_message("Manager stopped on detecting flag.")
 
 #----------main function----------
