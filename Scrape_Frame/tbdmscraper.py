51e70da13c9b511e7c251abef7a035f6484fc9c9
Mad s.o <Mad s.o>
2017-04-22 12:48:18 +0800

    modified the way to judge ju status

--- Scrape_Frame/tbdmscraper.py
+++ Scrape_Frame/tbdmscraper.py
@@ -199,24 +199,26 @@ class Worker():
             #     task['urlType'] = 4
             # else:
             #     task['urlType'] = 1
-            mix_time = re.findall('data-targettime="([0-9]{13})"', content)
-            if (re.findall('开抢', content)):
-                task['status'] = 1
-                task['score'] = int(mix_time[0]) // 1000
-            elif re.findall('还剩|马上抢|马上订', content):
-                task['status'] = 2
-                task['score'] = int(mix_time[0]) // 1000
-            elif (re.findall('已结束', content) or re.findall('卖光', content)):
+            if (re.findall('infotext J_Infotext', content) or re.findall('卖光', content)):
                 if (task['status'] < 3):
                     task['status'] = 3
             else:
-                worklog.error("juStatus-parsing error: " + str(_Eall) + " on task:" + str(task))
-                print(traceback.format_exc())
-                task['fail'] += 1
-                task['score'] = int(time.time() / 10) * 10 + PENALIZE_TIME
-                nfilename = datestr + '/error/juDetail-' + task['itemID'] + '-' + task['juID'] + '-' + str(int(time.time()))  + '.html'
-                self.save_gecko_page(nfilename, False)
-                return False
+                mix_time = re.findall('data-targettime="([0-9]{13})"', content)
+                if (re.findall('buyaction J_JuSMSRemind', content)):
+                    task['status'] = 1
+                    task['score'] = int(mix_time[0]) // 1000
+                elif re.findall('buyaction J_BuySubmit', content):
+                    task['status'] = 2
+                    task['score'] = int(mix_time[0]) // 1000
+                
+                else:
+                    worklog.error("juStatus-parsing error: " + str(_Eall) + " on task:" + str(task))
+                    print(traceback.format_exc())
+                    task['fail'] += 1
+                    task['score'] = int(time.time() / 10) * 10 + PENALIZE_TIME
+                    nfilename = datestr + '/error/juDetail-' + task['itemID'] + '-' + task['juID'] + '-' + str(int(time.time()))  + '.html'
+                    self.save_gecko_page(nfilename, False)
+                    return False
             nfilename = datestr + '/success/juDetail-' + task['itemID'] + '-' + task['juID'] + '-' + str(int(time.time()))  + '.html'
             self.save_gecko_page(nfilename, False)
             return True
