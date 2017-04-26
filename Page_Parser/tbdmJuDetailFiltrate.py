bea054fcfbd27f1402e8ae6aae14e544e8deaa1b
onns <onns@onns.xyz>
2017-04-26 10:40:49 -0400

    fox output

--- Page_Parser/tbdmJuDetailFiltrate.py
+++ Page_Parser/tbdmJuDetailFiltrate.py
@@ -118,7 +118,96 @@ juDetailXpath = {
 
 #----------function definition----------
 
+def parseJuDetailPage(htmlStr, htmlName):
+    treeObj = etree.HTML(htmlStr)
+    # Here we get a HTML tree so that we can use xpath to find the element we need.
 
+    for info in juDetailXpath:
+        # Find the information we need via the dict we declared.
+        isMatched = False
+        # Once we find the information, set the isMatched as True and then break out of the loop.
+        for i in range(len(juDetailXpath[info]['xpath'])):
+            # Find a useful xpath to get the information we need.
+            resultList = treeObj.xpath(juDetailXpath[info]['xpath'][i])
+            # The if statements below are used to make sure we match the right element as we predicted.
+            if(len(resultList) != 0):
+                if(juDetailXpath[info]['only'][i]):
+                    if(len(resultList) == 1 and resultList[0] != ''):
+                        juDetailResult[info] = resultList[0]
+                        isMatched = True
+                else:
+                    juDetailResult[info] = resultList
+                    isMatched = True
+            if(isMatched):
+                break
+        if(not(info in juDetailResult) and not(juDetailXpath[info]['option'])):
+            # The information we need but can not be found in juDetailResult
+            # So there must be some errors.
+            if(info == 'ju_price'):
+                # when the price is not an integer xpath can not match the right price
+                # so we need to use regular expression to get the price.
+                
+                ########################################
+                #               WARNING                #
+                ########################################
+                # watch out for the error IndexError.
+
+                juPriceRawStr = re.search('<span class="J_actPrice">([\S]+)</span>', htmlStr).group(0)
+                juPriceStr = juPriceRawStr.replace('<i>', '').replace('</i>', '').replace('<span class="J_actPrice">', '').replace('</span>', '')
+                juDetailResult['ju_price'] = juPriceStr
+                continue
+
+                ########################################
+                #               WARNING                #
+                ########################################
+            
+            # print the information for debuging.
+            print(htmlName)
+            print(info)
+            print('\033[1;31mMatch Error\033[0m')
+            return -1
+    # Do not forget to set ju_id and item_id that are stored in the filename.
+    juDetailResult['ju_id'] = htmlName.split('-')[1]
+    juDetailResult['item_id'] = htmlName.split('-')[2]
+    
+    # Here we have parsed all the useful data
+    # What we need to do next is to clean the data
+    
+    # From:     \n title \n
+    # To:       title
+    juDetailResult['title'] = juDetailResult['title'].strip()
+
+    # From:     background-image: url(head_picture_url);
+    # To:       head_picture_url
+    if(juDetailResult['head_picture'][0:16] == 'background-image'):
+        juDetailResult['head_picture'] = juDetailResult['head_picture'][22:-2]
+
+    # From:     ms
+    # To:       s
+    if('start_time' in juDetailResult):
+        juDetailResult['start_time'] = juDetailResult['start_time'][0:10]
+
+    # From:     \n ju_price \n
+    # To:       ju_price
+    juDetailResult['ju_price'] = juDetailResult['ju_price'].strip()
+    
+    # From:     ¥origin_price
+    # To:       origin_price
+    if('origin_price' in juDetailResult):
+        juDetailResult['origin_price'] = juDetailResult['origin_price'][1:]
+
+    # From:     ['rate ↑', 'rate -', 'rate ↓']
+    # To:       [['rate', '1'], ['rate', '0'], ['rate','-1']
+    for i in range(3):
+        if(juDetailResult['seller_rate'][i][-1:] == '↑'):
+            juDetailResult['seller_rate'][i] = [juDetailResult['seller_rate'][i][0:-2], '1']
+        if(juDetailResult['seller_rate'][i][-1:] == '-'):
+            juDetailResult['seller_rate'][i] = [juDetailResult['seller_rate'][i][0:-2], '0']
+        if(juDetailResult['seller_rate'][i][-1:] == '↓'):
+            juDetailResult['seller_rate'][i] = [juDetailResult['seller_rate'][i][0:-2], '-1']
+        
+
+    return juDetailResult
 
 #----------function definition----------
 
@@ -128,70 +217,19 @@ juDetailXpath = {
 if __name__ == "__main__":
     for date in os.listdir(fileLocation):
         # Filtrate the page day by day.
+        n = 0
         if(os.path.isdir(fileLocation + date) and len(date) == 8 and re.match('^([0-9]{8})$', date)):
             # Only if the path is a direction and the folder name is like YYYYMMDD can it be parsed.
             for juPage in os.listdir(fileLocation + date + '/success/'):
+                n = n + 1
                 juDetailResult = dict()
                 # the dict juDetailResult is used to store the content we parsed temporarily.
                 if(juPage[0:8] == 'juDetail'):
                     # Ju detail page will be named like juDetail-JuID-ItemID-Timestrap.html
                     pageObj = open(fileLocation + date + '/success/' + juPage, 'r', encoding='UTF-8')
                     pageStr = pageObj.read()
-                    treeObj = etree.HTML(pageStr)
-                    # Here we get a HTML tree so that we can use xpath to find the element we need.
-
-                    for info in juDetailXpath:
-                        # Find the information we need via the dict we declared.
-                        isMatched = False
-                        # Once we find the information, set the isMatched as True and then break out of the loop.
-                        for i in range(len(juDetailXpath[info]['xpath'])):
-                            # Find a useful xpath to get the information we need.
-                            resultList = treeObj.xpath(juDetailXpath[info]['xpath'][i])
-                            # The if statements below are used to make sure we match the right element as we predicted.
-                            if(len(resultList) != 0):
-                                if(juDetailXpath[info]['only'][i]):
-                                    if(len(resultList) == 1 and resultList[0] != ''):
-                                        juDetailResult[info] = resultList[0]
-                                        isMatched = True
-                                else:
-                                    juDetailResult[info] = resultList
-                                    isMatched = True
-                            if(isMatched):
-                                break
-                        if(not(info in juDetailResult) and not(juDetailXpath[info]['option'])):
-                            # The information we need but can not be found in juDetailResult
-                            # So there must be some errors.
-                            if(info == 'ju_price'):
-                                # when the price is not an integer xpath can not match the right price
-                                # so we need to use regular expression to get the price.
-                                
-                                ########################################
-                                #               WARNING                #
-                                ########################################
-                                # watch out for the error IndexError.
-
-                                juPriceRawStr = re.search('<span class="J_actPrice">([\S]+)</span>', pageStr).group(0)
-                                juPriceRawStr.replace('<i>', '').replace('</i>', '')
-                                juDetailResult['ju_price'] = juPriceRawStr
-                                continue
-
-                                ########################################
-                                #               WARNING                #
-                                ########################################
-                            
-                            # print the information for debuging.
-                            print(date)
-                            print(juPage)
-                            print(info)
-                            print('\033[1;31mMatch Error\033[0m')
-                    # Do not forget to set ju_id and item_id that are stored in the filename.
-                    juDetailResult['ju_id'] = juPage.split('-')[1]
-                    juDetailResult['item_id'] = juPage.split('-')[2]
-                    
-                    # Here we have parsed all the useful data
-                    # What we need to do next is to clean the data
-                    if(juDetailResult['head_picture'][0:16] == 'background-image'):
-                        juDetailResult['head_picture'] = juDetailResult['head_picture'][22:-2]
-                    print(juDetailResult)
+                    print(parseJuDetailPage(pageStr, juPage))
                 else:
                     continue
+                if(n == 10):
+                    break
