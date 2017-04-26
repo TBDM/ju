703ebf4a0436e9a82a1aeea0693ba443c32d2953
root <root@localhost.localdomain>
2017-04-26 08:24:43 -0400

    fix some bugs and add some notes

--- Page_Parser/tbdmJuDetailFiltrate.py
+++ Page_Parser/tbdmJuDetailFiltrate.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
-#Filtrate data from Ju detail pages.
+
+# Filtrate data from Ju detail pages.
+
+########################################
+#               WARNING                #
+########################################
+# Your HTML files must be list like this:
+#   root/         
+#   ...
+#       tbdm/
+#       ...
+#           file/
+#           ...
+#               20170526/
+#                   success/
+#                   error/
+#                   success.log
+#               20170525/
+#               20170524/
+#               20170523/
+#               ...
+# 
+# 
+# Set the global variable fileLocation 
+# as '/root/tbdm/file/'
+########################################
+#               WARNING                #
+########################################
 
 #----------model import----------
 
@@ -15,85 +42,71 @@ import re
 fileLocation = '/root/tbdm/file/'
 juDetailXpath = {
     'title': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//h2[@class="title"]/text()'], 
         'only': [True]
     }, 
     'type': {
-        'kind': 2, 
         'option': False, 
         'xpath': ['//div[@class="header clearfix"]/ul/li/a/text()', '//div[@class="header clearfix"]/a/img/@src'], 
         'only': [False, True]
     }, 
     'head_picture': {
-        'kind': 3, 
         'option': False, 
         'xpath': ['//div[@class="item-pic-wrap"]/img/@src', '//div[@class="J_zoom pic "]/@style', '//div[@class="J_zoom pic"]/@style'], 
         'only': [True, True, True]
     }, 
     'all_picture': {
-        'kind': 1, 
         'option': True, 
         'xpath': ['//ul[@class="thumbnails"]/li/img/@data-big'], 
         'only': [False]
     }, 
     'privilege': {
-        'kind': 2, 
         'option': False, 
         'xpath': ['//div[@class="biztag"]/label/text()', '//div[@class="biztag "]/label/text()'], 
         'only': [False, False]
     }, 
     'description': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//div[@class="description"]/ul/li/text()'], 
         'only': [False]
     }, 
     'start_time': {
-        'kind': 1, 
         'option': True, 
         'xpath': ['//div[@class="ju-clock J_juItemTimer"]/@data-targettime'], 
         'only': [True]
     }, 
     'ju_price': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//span[@class="extra currentPrice"]/span[@class="J_actPrice"]/text()'], 
         'only': [True]
     }, 
     'origin_price': {
-        'kind': 1, 
         'option': True, 
         'xpath': ['//del[@class="originPrice"]/text()'], 
         'only': [True]
     }, 
     'sale': {
-        'kind': 1, 
         'option': True, 
         'xpath': ['//span[@class="soldnum"]/em/text()'], 
         'only': [True]
     }, 
     'seller_name': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//div[@class="tit  J_sellerInfoTit"]/a/text()'], 
         'only': [True]
     }, 
     'seller_url': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//div[@class="tit  J_sellerInfoTit"]/a/@href'], 
         'only': [True]
     }, 
     'seller_rate': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//div[@class="con"]/table/tbody/tr[2]/td/text()'], 
         'only': [False]
     }, 
     'seller_promise': {
-        'kind': 1, 
         'option': False, 
         'xpath': ['//div[@class="con"]/ul[@class="clearfix J_PromiseCon"]/li/a/span/text()'], 
         'only': [False]
@@ -114,42 +127,71 @@ juDetailXpath = {
 
 if __name__ == "__main__":
     for date in os.listdir(fileLocation):
-        #Filtrate the page day by day.
-        for juPage in os.listdir(fileLocation + date + '/success/'):
-            juDetailResult = dict()
-            if(juPage[0:8] == 'juDetail'):
-                pageObj = open(fileLocation + date + '/success/' + juPage, 'r', encoding='UTF-8')
-                pageStr = pageObj.read()
-                treeObj = etree.HTML(pageStr)
-                
-                for info in juDetailXpath:
-                    isMatched = False
-                    for i in range(juDetailXpath[info]['kind']):
-                        resultList = treeObj.xpath(juDetailXpath[info]['xpath'][i])
-                        if(len(resultList) != 0):
-                            if(juDetailXpath[info]['only'][i]):
-                                if(len(resultList) == 1 and resultList[0] != ''):
-                                    juDetailResult[info] = resultList[0]
+        # Filtrate the page day by day.
+        if(os.path.isdir(fileLocation + date) and len(date) == 8 and re.match('^([0-9]{8})$', date)):
+            # Only if the path is a direction and the folder name is like YYYYMMDD can it be parsed.
+            for juPage in os.listdir(fileLocation + date + '/success/'):
+                juDetailResult = dict()
+                # the dict juDetailResult is used to store the content we parsed temporarily.
+                if(juPage[0:8] == 'juDetail'):
+                    # Ju detail page will be named like juDetail-JuID-ItemID-Timestrap.html
+                    pageObj = open(fileLocation + date + '/success/' + juPage, 'r', encoding='UTF-8')
+                    pageStr = pageObj.read()
+                    treeObj = etree.HTML(pageStr)
+                    # Here we get a HTML tree so that we can use xpath to find the element we need.
+
+                    for info in juDetailXpath:
+                        # Find the information we need via the dict we declared.
+                        isMatched = False
+                        # Once we find the information, set the isMatched as True and then break out of the loop.
+                        for i in range(len(juDetailXpath[info]['xpath'])):
+                            # Find a useful xpath to get the information we need.
+                            resultList = treeObj.xpath(juDetailXpath[info]['xpath'][i])
+                            # The if statements below are used to make sure we match the right element as we predicted.
+                            if(len(resultList) != 0):
+                                if(juDetailXpath[info]['only'][i]):
+                                    if(len(resultList) == 1 and resultList[0] != ''):
+                                        juDetailResult[info] = resultList[0]
+                                        isMatched = True
+                                else:
+                                    juDetailResult[info] = resultList
                                     isMatched = True
-                            else:
-                                juDetailResult[info] = resultList
-                                isMatched = True
-                        if(isMatched):
-                            break
-                    if(not(info in juDetailResult) and not(juDetailXpath[info]['option'])):
-                        if(info == 'ju_price'):
-                            juPriceRawStr = re.search('<span class="J_actPrice">([\S]+)</span>', pageStr).group(0)
-                            juPriceRawStr.replace('<i>', '').replace('</i>', '')
-                            juDetailResult['ju_price'] = juPriceRawStr
-                            continue
-                        print(date)
-                        print(juPage)
-                        print(info)
-                        print('\033[1;31mMatch Error\033[0m')
-                juDetailResult['ju_id'] = juPage.split('-')[1]
-                juDetailResult['item_id'] = juPage.split('-')[2]
-                if(juDetailResult['head_picture'][0:16] == 'background-image'):
-                    juDetailResult['head_picture'] = juDetailResult['head_picture'][22:-2]
-                print(juDetailResult)
-            else:
-                continue
+                            if(isMatched):
+                                break
+                        if(not(info in juDetailResult) and not(juDetailXpath[info]['option'])):
+                            # The information we need but can not be found in juDetailResult
+                            # So there must be some errors.
+                            if(info == 'ju_price'):
+                                # when the price is not an integer xpath can not match the right price
+                                # so we need to use regular expression to get the price.
+                                
+                                ########################################
+                                #               WARNING                #
+                                ########################################
+                                # watch out for the error IndexError.
+
+                                juPriceRawStr = re.search('<span class="J_actPrice">([\S]+)</span>', pageStr).group(0)
+                                juPriceRawStr.replace('<i>', '').replace('</i>', '')
+                                juDetailResult['ju_price'] = juPriceRawStr
+                                continue
+
+                                ########################################
+                                #               WARNING                #
+                                ########################################
+                            
+                            # print the information for debuging.
+                            print(date)
+                            print(juPage)
+                            print(info)
+                            print('\033[1;31mMatch Error\033[0m')
+                    # Do not forget to set ju_id and item_id that are stored in the filename.
+                    juDetailResult['ju_id'] = juPage.split('-')[1]
+                    juDetailResult['item_id'] = juPage.split('-')[2]
+                    
+                    # Here we have parsed all the useful data
+                    # What we need to do next is to clean the data
+                    if(juDetailResult['head_picture'][0:16] == 'background-image'):
+                        juDetailResult['head_picture'] = juDetailResult['head_picture'][22:-2]
+                    print(juDetailResult)
+                else:
+                    continue
