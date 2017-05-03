ca2b255f40af1e4bcbed4590a520fde9431b2364
onns <onns@onns.xyz>
2017-05-03 23:04:29 +0800

    edit logposter

--- Scrape_Frame/tbdmLogPoster.py
+++ Scrape_Frame/tbdmLogPoster.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
-#coding:utf-8
+# -*- coding: utf-8 -*-
 
 import socket
 import time
+import sys
+
+if(len(sys.argv) != 3):
+	exit(1)
+
+toUserName = sys.argv[1]
+message = sys.argv[2]
 
 port = 8888
 #host = '123.206.176.165'
 host = '139.199.77.204'
 
+
+
+
 def post_log(whoami, loginfo):
-	print('ok')
+	# print('ok')
 	try:
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sock.sendto(whoami.encode() + b'\n: ' + loginfo.encode(), (host,8888))
+		exit(0)
 	except Exception as _Eall:
 		print(str(_Eall))
-	print('ok')
+		exit(1)
+	# print('ok')
 
 if __name__ == '__main__':
-	post_log('XZLiu', 'Oops! Pan said that I must tell you I have requseted a login page!')
+	post_log(toUserName, message)
