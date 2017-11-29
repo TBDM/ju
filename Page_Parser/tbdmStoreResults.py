75f018d19feda1a8cf952185768fc3d68f40a9c5
Mads <lxzmads@gmail.com>
2017-11-29 22:54:18 +0800

    Modified the way to store.

--- Page_Parser/tbdmStoreResults.py
+++ Page_Parser/tbdmStoreResults.py
@@ -4,25 +4,31 @@
 import json
 import sys
 import traceback
+import time
 
 sys.path.append('../')
 
 from Conf.tbdmSetting import tbdmDatabase
 
 
-def storeResult(db, file):
+def storeResult(db, file, type):
     item_num = 0
+    if(type == 'ju'):
+        collection = 'ju_detail'
+    elif(type == 'item'):
+        collection = 'item_detail'
     with open(file, 'r', encoding='UTF-8') as f:
         try:
-            bulk = db['JuDetail'].initialize_unordered_bulk_op()
+            bulk = db[collection].initialize_unordered_bulk_op()
             for line in f:
                 item = json.loads(line)
-                if(not db['ItemDetail'].find_one({'item_id' : item['item_id'], 'ju_id': item['ju_id']})):
-                    print('hit: ' + item['ju_id'])
-                    item_num += 1
-                    bulk.insert(item)
-                else:
-                    print(item['item_id']+'already in database.')
+                if(type == 'ju'):
+                    print('Storing ju page '+item['ju_id'])
+                    bulk.find({'ju_id': item['ju_id'], 'item_id': item['item_id'], 'timestamp': item['timestamp']}).upsert().update_one({'$setOnInsert':item})
+                elif(type == 'item'):
+                    print('Storing item page ' + item['item_id'])
+                    bulk.find({'item_id': item['item_id'], 'timestamp': item['timestamp']}).upsert().update({'$setOnInsert':item})
+                item_num += 1
         except Exception as _Eall:
             traceback.print_exc()
         try:
@@ -33,11 +39,14 @@ def storeResult(db, file):
     return item_num
 
 if __name__ == '__main__':
-    if(not len(sys.argv[1:])):
-        print('Usage: '+sys.argv[0]+' [origin file]')
+    # fileLocation = './test-ju.json'
+    # type = 'ju'
+    if(not len(sys.argv[2:])):
+        print('Usage: '+sys.argv[0]+' [origin file] [page type]')
         sys.exit(0)
     fileLocation = sys.argv[1]
+    type = sys.argv[2]
     tbdmDb = tbdmDatabase()
     mongod = tbdmDb.tbdmMongo(addrOwner = 'xmu_local', authDb = 'tbdm')
-    res = storeResult(mongod, fileLocation)
-    print("Finish " + str(res))
+    res = storeResult(mongod, fileLocation, type)
+    print("Finish " + str(res) + 'At: '+time.strftime('%Y-%m-%d-%H:%M', time.localtime(time.time())))
